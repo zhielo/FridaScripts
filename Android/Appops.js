ca717739766ed626b2344680a0bb21111d6d8206
Apkunpacker <27184655+apkunpacker@users.noreply.github.com>
2020-03-31 08:32:19 +0530

    Create Appops.js

---
+++ Android/Appops.js
@@ -0,0 +1,20 @@
+ setImmediate(function() 
+{
+   console.log("[*] Helper Script for Appops 3.1.1.r956.eec61b4...........");
+   Java.perform ( function() 
+   {  
+      
+	
+       var Ops =Java.use("rikka.appops.xB");
+       Ops["诶诶诶"].overload().implementation = function ()
+        {
+         return true ;
+        }
+	   
+	  
+
+
+	}
+  )
+ }
+);
